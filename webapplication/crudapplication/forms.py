2adbe2ff43f74aeb051c5519489e03611339d196
alirangwala <Karbala@786@52>
2019-11-02 16:49:52 +0530

    web app module form

--- webapplication/crudapplication/forms.py
+++ webapplication/crudapplication/forms.py
@@ -1,3 +1,7 @@
 from django import forms
 from crudapplication.models import Employee
 
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = "__all__" # This denotes that we need all fields of the model
